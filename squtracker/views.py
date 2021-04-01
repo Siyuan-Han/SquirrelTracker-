758e10c552bff197b6197e07f6cf7d26d39e9011
Siyuan Han <sh4156@columbia.edu>
2021-04-01 16:36:00 +0000

    update

--- squtracker/views.py
+++ squtracker/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views here.
+def map_(request):
+   # sightings= Squirrel.objects.all()[:100]
+   # context ={
+   #         'sightings':sightings,
+   #          }
+    return render(request, 'squtracker/map.html', context)
+
