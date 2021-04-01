1c270320ed73195bcbb06f8c830f71162fbf70c1
Siyuan-Han <81219965+Siyuan-Han@users.noreply.github.com>
2021-04-02 00:39:58 +0800

    Update views.py

--- squtracker/views.py
+++ squtracker/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import render,redirect
 
-def map_(request):
+def get_map(request):
    # sightings= Squirrel.objects.all()[:100]
    # context ={
    #         'sightings':sightings,
    #          }
-    return render(request, 'squtracker/map.html', context)
+    return render(request, 'squtracker/map.html', {}) # replace {} by content
 
