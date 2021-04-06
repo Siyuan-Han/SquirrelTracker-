ab3190639ad16b04242dbfbbc2302822267c7d33
Siyuan Han <sh4156@columbia.edu>
2021-04-06 13:00:56 +0000

    add two new html

--- squtracker/urls.py
+++ squtracker/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name ='squtracker'
+
 urlpatterns = [
+        path('',views.mainpage),
         path('map/',views.get_map),
         path('sightings/',views.get_sightings),
+        path('sightings/<Unique_squirrel_ID>/', views.detail)
     ]
 
--- squtracker/views.py
+++ squtracker/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
-from django.shortcuts import render,redirect
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 from .models import Squirrel
 
 def get_map(request):
     sightings= Squirrel.objects.all()[:100]
     context ={
-             'sightings':sightings
+             'sightings':sightings,
               }  
     return render(request, 'squtracker/map.html', context)
 
 def get_sightings(request):
     squirrels = Squirrel.objects.all()
     context ={
-             'squirrels':squirrels
-              }
+              'squirrels':squirrels,
+               }
     return render(request, 'squtracker/sighsighs.html', context)
+
+def detail(request,Unique_squirrel_ID):
+    squirrel = get_object_or_404(Squirrel, pk =Unique_squirrel_ID)
+    context ={
+              'squirrel':squirrel,
+               }
+    return render(request, 'squtracker/squdetail.html', context)
+def mainpage(request):
+    return render(request, 'squtracker/mainpage.html')
 
